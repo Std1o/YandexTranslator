ee296d9f81c9c39357d4463e97ddfb4f54011af1
Stdio <kwork-stdio@mail.ru>
2019-07-24 17:34:29 +0300

    0.0.1

--- app/src/main/java/com/stdio/yandextranslator/MainActivity.java
+++ app/src/main/java/com/stdio/yandextranslator/MainActivity.java
@@ -3,12 +3,81 @@ package com.stdio.yandextranslator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et;
+    TextView tv;
+    String result;
+    //API запросов написан тут https://tech.yandex.ru/translate/doc/dg/reference/translate-docpage/
+    private final String KEY = "trnsl.1.1.20180923T185306Z.c0c12e833188400b.99507f30e616465853f697c9b2961fac934fd5aa";// API ключ получить можно здесь https://translate.yandex.ru/developers/keys
+    private final String URL = "https://translate.yandex.net/api/v1.5/tr.json/translate";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void translate() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            result = jsonObject.getString("text")
+                                    .replace("[", "").replace("]", "");
+                            result = result.replaceAll("\"", "");
+                            tv.setText(result);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("key", KEY);
+                params.put("text", et.getText().toString());
+                params.put("lang", "en-ru");
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public void onClick(View view) {
+        et = findViewById(R.id.et);
+        tv = findViewById(R.id.tv);
+        translate();
     }
 }
